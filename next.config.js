b764cbbe10457bee6eb23ea31dbed46ed2ebe8c7
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-29 13:31:52 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ module.exports = {
     images:{
         domains:["www.reviewpro.com","www.google.com"],
     },
-    env:{
-        API_KEY:'AIzaSyD46JUVG7O2uXpehKej_UGXTHTd8iV7iU8',
-        CONTEXT_KEY:'225dcfba0b5477436'
-    }
 }
